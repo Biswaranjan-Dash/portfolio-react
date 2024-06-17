e4ffafc0119578628e779be046e7c4af9c8d8aa8
biswaranjan-dash <nitrbiswa@gmail.com>
2024-06-17 19:33:42 +0530

    new parallax

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -11,7 +11,10 @@ const Parallax = ({ type }) => {
   });
 
   const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
-  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const scaletxt = useTransform(scrollYProgress, [0, 1], [1, 0.1]);
+  const opa = useTransform(scrollYProgress, [0, 1], ["100%", "-100%"]);
+  const scaleBg = useTransform(scrollYProgress, [0, 1], [1, 3]);
+  const scaleup = useTransform(scrollYProgress, [0, 1], [1, 1.7]);
 
   return (
     <div
@@ -24,20 +27,12 @@ const Parallax = ({ type }) => {
             : "linear-gradient(180deg, #111132, #505064)",
       }}
     >
-      <motion.h1 style={{ y: yText }}>
+      <motion.h1 style={{ y: yText ,scale:scaletxt}}>
         {type === "services" ? "My Expertise!!" : "My Works!!"}
       </motion.h1>
-      <motion.div className="mountains"></motion.div>
-      <motion.div
-        className="planets"
-        style={{
-          y: yBg,
-          backgroundImage: `url(${
-            type === "services" ? "/planets.png" : "/sun.png"
-          })`,
-        }}
-      ></motion.div>
-      <motion.div style={{ x: yBg }} className="stars"></motion.div>
+      <motion.div style={{scale:scaleup,opacity:opa }} className="mountains"></motion.div>
+      
+      <motion.div style={{scale:scaleBg}} className="stars"></motion.div>
     </div>
   );
 };
